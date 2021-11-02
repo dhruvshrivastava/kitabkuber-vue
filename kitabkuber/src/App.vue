a65d7ee29308ad2ce930d1847726f5ec02824eb7
Dhruv Shrivastava <dhruvsri5@gmail.com>
2021-11-02 15:53:51 +0530

    Latest Products page

--- kitabkuber/src/App.vue
+++ kitabkuber/src/App.vue
@@ -92,66 +92,13 @@
 
             <!-- Logo -->
             <div class="ml-4 flex lg:ml-0">
-              <a href="#">
+              <a href="/">
                 <span class="sr-only">Workflow</span>
                 <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="" />
               </a>
             </div>
 
-            <!-- Flyout menus -->
-            <PopoverGroup class="hidden lg:ml-8 lg:block lg:self-stretch">
-              <div class="h-full flex space-x-8">
-                <Popover v-for="category in navigation.categories" :key="category.name" class="flex" v-slot="{ open }">
-                  <div class="relative flex">
-                    <PopoverButton :class="[open ? 'border-green-600 text-green-600' : 'border-transparent text-gray-700 hover:text-gray-800', 'relative z-10 flex items-center transition-colors ease-out duration-200 text-sm font-medium border-b-2 -mb-px pt-px']">
-                      {{ category.name }}
-                    </PopoverButton>
-                  </div>
-
-                  <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0" enter-to-class="opacity-100" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100" leave-to-class="opacity-0">
-                    <PopoverPanel class="absolute top-full inset-x-0 text-sm text-gray-500">
-                      <!-- Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow -->
-                      <div class="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
-
-                      <div class="relative bg-white">
-                        <div class="max-w-7xl mx-auto px-8">
-                          <div class="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
-                            <div class="col-start-2 grid grid-cols-2 gap-x-8">
-                              <div v-for="item in category.featured" :key="item.name" class="group relative text-base sm:text-sm">
-                                <div class="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden group-hover:opacity-75">
-                                  <img :src="item.imageSrc" :alt="item.imageAlt" class="object-center object-cover" />
-                                </div>
-                                <a :href="item.href" class="mt-6 block font-medium text-gray-900">
-                                  <span class="absolute z-10 inset-0" aria-hidden="true" />
-                                  {{ item.name }}
-                                </a>
-                                <p aria-hidden="true" class="mt-1">Shop now</p>
-                              </div>
-                            </div>
-                            <div class="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
-                              <div v-for="section in category.sections" :key="section.name">
-                                <p :id="`${section.name}-heading`" class="font-medium text-gray-900">
-                                  {{ section.name }}
-                                </p>
-                                <ul role="list" :aria-labelledby="`${section.name}-heading`" class="mt-6 space-y-6 sm:mt-4 sm:space-y-4">
-                                  <li v-for="item in section.items" :key="item.name" class="flex">
-                                    <a :href="item.href" class="hover:text-gray-800">
-                                      {{ item.name }}
-                                    </a>
-                                  </li>
-                                </ul>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </PopoverPanel>
-                  </transition>
-                </Popover>
-
-                <a v-for="page in navigation.pages" :key="page.name" :href="page.href" class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">{{ page.name }}</a>
-              </div>
-            </PopoverGroup>
+         
 
             <div class="ml-auto flex items-center">
               <div class="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
@@ -162,13 +109,14 @@
 
               <!-- Search -->
               <div class="flex lg:ml-6">
-                <a href="#" class="p-2 text-gray-400 hover:text-gray-500">
+                <a href="" class="p-2 text-gray-400 hover:text-gray-500">
                   <span class="sr-only">Search</span>
                   <SearchIcon class="w-6 h-6" aria-hidden="true" />
                 </a>
               </div>
             </div>
           </div>
+          <router-view></router-view>
         </div>
       </nav>
     </header>
@@ -195,56 +143,7 @@ import {
 import { MenuIcon, SearchIcon, XIcon } from '@heroicons/vue/outline'
 
 const navigation = {
-  categories: [
-    {
-      id: 'catalog',
-      name: 'Browse Catalog',
-      sections: [
-        {
-          id: 'sat',
-          name: 'SAT',
-          items: [
-            { name: 'Rent SAT books', href: '/sat/' },
-          ],
-        },
-        {
-          id: 'iit-jee',
-          name: 'IIT-JEE',
-          items: [
-            { name: 'Rent IIT-JEE books', href: '/iit-jee/' },
-          ],
-        },
-        {
-          id: 'gmat',
-          name: 'GMAT',
-          items: [
-            { name: 'Rent GMAT books', href: '/gmat/' },
-          ],
-        },
-        {
-          id: 'gre',
-          name: 'GRE',
-          items: [
-            { name: 'Rent GRE books', href: '/gre/' },
-          ],
-        },
-        {
-          id: 'ielts',
-          name: 'IELTS',
-          items: [
-            { name: 'Rent IELTS books', href: '/ielts/' },
-          ],
-        },
-        {
-          id: 'toefl',
-          name: 'TOEFL',
-          items: [
-            { name: 'Rent TOEFL books', href: '/toefl/' },
-          ],
-        },
-      ],
-    },
-  ],
+
   pages: [
     { name: 'Rental Plans', href: '/plans/' },
   ],
---
+++ kitabkuber/src/views/Home.vue
@@ -0,0 +1,65 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="relative bg-white overflow-hidden">
+    <div class="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+      <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
+        <div class="sm:max-w-lg">
+          <h1 class="text-4xl font font-extrabold tracking-tight text-gray-900 sm:text-6xl">
+            Looking to Rent Books?
+          </h1>
+          <h1 class="text-4xl font font-extrabold tracking-tight text-green-900 sm:text-6xl">
+            Here's KitabKuber
+          </h1>
+          <p class="mt-4 text-xl text-gray-500">KitabKuber lets you buy or rent exam preparation books at affordable rates. Our rental 
+            plans are exclusively curated for students so that they can focus on their exams and save big. 
+          </p>
+        </div>
+        <div>
+          <div class="mt-10">
+            <!-- Decorative image grid -->
+            <div aria-hidden="true" class="pointer-events-none lg:absolute lg:inset-y-0 lg:max-w-7xl lg:mx-auto lg:w-full">
+              <div class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                <div class="flex items-center space-x-8 lg:space-x-8">
+                  <div class="flex-shrink-0 grid grid-cols-2 gap-y-6 lg:gap-y-8">
+                    <div class="w-44 h-full rounded-lg overflow-hidden sm:opacity-0 lg:opacity-100">
+                     <img src="../assets/ielts.png" alt="" class="object-center object-cover" />
+                    </div>
+                    <div class="w-45 h-full rounded-lg overflow-hidden">
+                      <img src="../assets/jee.jpeg" alt="" class="object-center object-cover" />
+                    </div>
+                    <div class="w-44 h-full rounded-lg overflow-hidden">
+                      <img src="../assets/toefl.png" alt="" class=" object-center object-cover" />
+                    </div>
+
+                    <div class="w-44 h-full rounded-lg overflow-hidden">
+                      <img src="../assets/gmat.png" alt="" class="object-center object-cover" />
+                    </div>
+                    <div class="w-45 h-full rounded-lg overflow-hidden">
+                      <img src="../assets/gre.png" alt="" class=" object-center object-cover" />
+                    </div>
+                    <div class="w-44 h-full rounded-lg overflow-hidden">
+                       <img src="../assets/sat.png" alt="" class=" w-25 h-25 object-center object-cover" />
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+        <router-link to = "/books/"> <a href="/books/" class="inline-block text-center bg-green-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-green-700">Browse Catalog</a> </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home'
+}
+
+
+</script>
